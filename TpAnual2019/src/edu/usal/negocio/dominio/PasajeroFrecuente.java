4c8c9e3653311a8185dbd1255e462608eaee5935
GB <g.billinghurst@usal.edu.ar>
2019-05-01 12:47:15 -0300

    2

---
+++ TpAnual2019/src/edu/usal/negocio/dominio/PasajeroFrecuente.java
@@ -0,0 +1,19 @@
+package edu.usal.negocio.dominio;
+
+public class PasajeroFrecuente {
+	private String Aerolinea;
+	private String Numero;
+	
+	public String getAerolinea() {
+		return Aerolinea;
+	}
+	public void setAerolinea(String aerolinea) {
+		Aerolinea = aerolinea;
+	}
+	public String getNumero() {
+		return Numero;
+	}
+	public void setNumero(String numero) {
+		Numero = numero;
+	}
+}
