a8982a1e8acccf3bed8f4448a6fe81822b92eecc
augus <augus@DESKTOP-6PEVTJM>
2019-05-10 19:08:55 -0300

    LineasAereas sin implementaciones

---
+++ TpAnual2019/src/edu/usal/negocio/dao/interfaces/LineasAereasDAO.java
@@ -0,0 +1,5 @@
+package edu.usal.negocio.dao.interfaces;
+
+public class LineasAereasDAO {
+
+}
---
+++ TpAnual2019/src/edu/usal/negocio/dao/interfaces/RegistroLineasAereasDAO.java
@@ -0,0 +1,5 @@
+package edu.usal.negocio.dao.interfaces;
+
+public interface RegistroLineasAereasDAO {
+
+}
