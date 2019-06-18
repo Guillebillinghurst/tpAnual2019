76e6ab6f207245b8642bcf42334c0350822f5f97
Francisco Villanueva <Francisco Villanueva@PC-Francisco>
2019-06-18 20:45:03 -0300

    Cliente BD

---
+++ TpAnual2019/src/edu/usal/negocio/dao/implementacion/ClienteDAOimplBD.java
@@ -0,0 +1,23 @@
+package edu.usal.negocio.dao.implementacion;
+
+import edu.usal.negocio.dao.interfaces.ClienteDAO;
+import edu.usal.negocio.dominio.Cliente;
+
+public class ClienteDAOimplBD {
+	
+	public boolean alta (Cliente cliente) {
+		return false;
+	}
+	
+	public boolean baja (int id) {
+		return false;
+	}
+	
+	public boolean modificiacion (int id, Cliente cliente) {
+		return false;
+	}
+	
+	public Cliente consulta (int id) {
+		return null;
+	}
+}
